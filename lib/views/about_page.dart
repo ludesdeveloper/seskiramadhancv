680c33fdaf02bb4f26365ce580880b18bc667491
Seski Ramadhan <seski.ramadhan@gmail.com>
2020-09-10 22:10:15 +0700

    update content

--- lib/views/about_page.dart
+++ lib/views/about_page.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('About Page'));
+    return Center(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Seski Ramadhan',
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'DevOps | Cloud Enthusiast | CCNP Collaboration | Python Coder | Flutter & Firebase Enthusiast',
+          style: TextStyle(fontSize: 17),
+        ),
+      ],
+    ));
   }
 }
